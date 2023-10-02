7ca3f640d0b57a3836769e8c94485c3be77428be
David Ambrozio <davidambroziodev@gmail.com>
2023-10-01 21:26:06 -0300

    remove route first

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ from views import user_router, assets_router
 app = FastAPI()
 router = APIRouter()
 
-
-@router.get('/')
-def first():
-    return 'Hello world!'
-
 app.include_router(prefix='/first', router=router)
 app.include_router(user_router)
 app.include_router(assets_router)
